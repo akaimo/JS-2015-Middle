6648b010cf5973be4747ff00e518a68d85839ed2
shuhei <kawashu0723@gmail.com>
2015-06-02 19:39:16 +0900

    fin kadai2

---
+++ javascripts/application.js
@@ -0,0 +1,60 @@
+$(function() {
+    var array = [];
+    var count = 0;
+    // 追加ボタンをクリックしたとき
+    $("#submit").click(function(){
+        var data = {
+            name:$("input[name='name']").val(),
+            ken:$("input[name='ken']").val(),
+            siku:$("input[name='siku']").val(),
+            tyou:$("input[name='tyou']").val(),
+            date:new Date($("input[name='date']").val())
+        };
+        array.push(data);
+        addRow(data, count);
+        addSelect(data);
+        count += 1;
+    });
+
+    // 都道府県ソートを変更したとき
+    $('#sortSelect').change(function() {
+        hideRow(array);
+    });
+});
+
+
+// 行を追加する
+var addRow = function(data, count) {
+    $('#addressTable').append("<tr id="+count+"><td>" + data.name + "</td><td>" + data.ken + data.siku + data.tyou +
+    "</td><td>" + formatDate(data.date) + "</td></tr>");
+}
+
+// Date型をフォーマット
+var formatDate = function toLocaleString(date){
+    return [date.getFullYear(), date.getMonth() + 1, date.getDate()].join( '/' ) + ' '
+}
+
+// selectのoptionに存在しない場合は追加
+var addSelect = function(data) {
+  if ($("#sortSelect option[value=" + data.ken + "]").size() == 0) {
+      $('#sortSelect').append($('<option>').html(data.ken).val(data.ken));
+  };
+}
+
+// selectedと一致する行を非表示
+var hideRow = function(array) {
+    var selected = $("#sortSelect option:selected").text();
+    if (selected == "全て") {
+        // 全てのときは、表を全て表示
+        for (var i=0; i < array.length; i++) {
+            $("#addressTable #"+i).show();
+        };
+    } else {
+        // 一致する行のみ表示
+        for (var i=0; i < array.length; i++) {
+            if (array[i].ken != selected) $("#addressTable #"+i).hide();
+            else                          $("#addressTable #"+i).show();
+        };
+    }
+}
+
