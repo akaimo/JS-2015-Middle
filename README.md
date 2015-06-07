b052a8a31186e9a8efca4f55a64acd02269cfe10
shuhei <kawashu0723@gmail.com>
2015-06-07 20:31:54 +0900

    refactoring

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# hoge  
-hoge  
-- hoge  
-- hoge  
+## オブジェクト指向技術 中間課題
+2015年オブジェクト指向技術の中間課題リポジトリ  
+
+### 開発環境
+- Mac OS X 10.10.3
+- jQuery 2.1.4
+- Sublime Text 3
+- UTF-8
+
+### 使い方
+実行例  
+
+```JavaScript
+    % open Address.html
+```
+
 
--- javascripts/application.js
+++ javascripts/application.js
@@ -1,43 +1,41 @@
 $(function() {
-    var array = [];
-    var count = 0;
+    var rowArray = [];
+    var rowCount = 0;
     // 追加ボタンをクリックしたとき
-    $("#submit").click(function(){
+    $('#submit').click(function(){
         var data = {
-            name:$("input[name='name']").val(),
-            ken:$("input[name='ken']").val(),
-            siku:$("input[name='siku']").val(),
-            tyou:$("input[name='tyou']").val(),
-            date:new Date($("input[name='date']").val()),
+            name:$('input[name="name"]').val(),
+            prefecture:$('input[name="prefecture"]').val(),
+            city:$('input[name="city"]').val(),
+            town:$('input[name="town"]').val(),
+            birthday:new Date($('input[name="birthday"]').val()),
             old:function() {
-                return parseInt((formatOld(new Date) - formatOld(this.date)) / 10000);
+                return parseInt((formatForOld(new Date) - formatForOld(this.birthday)) / 10000);
             }
         };
-        array.push(data);
-        addRow(data, count);
-        addSelect(data);
-        count += 1;
+        rowArray.push(data);
+        addRow(data, rowCount);
+        addSelectOption(data);
+        rowCount += 1;
     });
 
     // 都道府県ソートを変更したとき
-    $('#sortSelect').change(function() {
-        hideRow(array);
+    $('#sortPrefecture').change(function() {
+        sortPrefecture(rowArray);
     });
 
     // 誕生日ソートを変更したとき
     $('#sortDate').change(function() {
-        sortDate(array);
+        sortDate(rowArray);
     });
 });
 
 
-// 行を追加する
 var addRow = function(data, count) {
-    // $('#addressTable').fadeOut(0);
-    $('#addressTable').append("<tr id="+count+"><td>" + data.name + "</td><td>" + data.ken + data.siku + data.tyou +
-    "</td><td>" + formatDate(data.date) + "</td><td>" + data.old() + "</td></tr>");
-    $('#'+count).hide();
-    $('#'+count).fadeIn("slow");
+    $('#addressTable').append('<tr id=row'+count+'><td>' + data.name + '</td><td>' + data.prefecture + data.city + data.town +
+    '</td><td>' + formatDate(data.birthday) + '</td><td>' + data.old() + '</td></tr>');
+    $('#row'+count).hide();
+    $('#row'+count).fadeIn('slow');
 }
 
 // Date型をフォーマット
@@ -46,7 +44,7 @@ var formatDate = function toLocaleString(date){
 }
 
 // 年齢計算用のフォーマット
-var formatOld = function toLocaleString(date){
+var formatForOld = function toLocaleString(date){
     var month = date.getMonth() + 1;
     var day = date.getDate();
     if (month < 10) {month = '0' + month;};
@@ -54,54 +52,51 @@ var formatOld = function toLocaleString(date){
     return date.getFullYear() + month + day;
 }
 
-// selectのoptionに存在しない場合は追加
-var addSelect = function(data) {
-  if ($("#sortSelect option[value=" + data.ken + "]").size() == 0) {
-      $('#sortSelect').append($('<option>').html(data.ken).val(data.ken));
+var addSelectOption = function(data) {
+  if ($('#sortPrefecture option[value=' + data.prefecture + ']').size() == 0) {
+      $('#sortPrefecture').append($('<option>').html(data.prefecture).val(data.prefecture));
   };
 }
 
 // selectedと一致する行を非表示
-var hideRow = function(array) {
-    var selected = $("#sortSelect option:selected").text();
-    if (selected == "全て") {
-        // 全てのときは、表を全て表示
+var sortPrefecture = function(array) {
+    var selected = $('#sortPrefecture option:selected').text();
+    if (selected == '全て') {
+        // 全ての行を表示
         for (var i=0; i < array.length; i++) {
-            $("#addressTable #"+i).fadeIn("slow");
+            $('#addressTable #row'+i).fadeIn('slow');
         };
     } else {
         // 一致する行のみ表示
         for (var i=0; i < array.length; i++) {
-            if (array[i].ken != selected) $("#addressTable #"+i).hide();
-            else                          $("#addressTable #"+i).fadeIn("slow");
+            if (array[i].prefecture != selected) $('#addressTable #row'+i).hide();
+            else                                 $('#addressTable #row'+i).fadeIn('slow');
         };
     }
 }
 
-// 誕生日でソートする
 var sortDate = function(array) {
-    var selected = $("#sortDate option:selected").text();
-    if (selected == "昇順") {
+    var selected = $('#sortDate option:selected').text();
+    if (selected == '昇順') {
         array.sort(function(a,b){
-            return a.date - b.date;
+            return a.birthday - b.birthday;
         });
         rewriteTable(array);
-    } else if (selected == "降順") {
+    } else if (selected == '降順') {
         array.sort(function(a,b){
-            return b.date - a.date;
+            return b.birthday - a.birthday;
         });
         rewriteTable(array);
     };
 }
 
-// 表を書き換える
 var rewriteTable = function(array) {
     for (var i=0; i < array.length; i++) {
-        $("#addressTable #"+i).remove();
+        $('#addressTable #row'+i).remove();
     };
     for (var i=0; i < array.length; i++) {
         addRow(array[i], i);
     };
-    hideRow(array);
+    sortPrefecture(array);
 }
 
